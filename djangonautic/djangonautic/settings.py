47f3dbf74089370ad9613db8a60f6cab40157afd
Aroooba <arooba.shahoor@gmail.com>
2024-04-29 18:21:41 +0900

    static files config

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = ["static_assets"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -130,11 +129,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL='/myimages/'
 MEDIA_ROOT= path.join(BASE_DIR, 'myimages')
 
+
+STATIC_URL = "static/"
+# STATICFILES_DIRS = ["static_assets"]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_assets')
