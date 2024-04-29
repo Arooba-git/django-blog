fda8c2c0836260143499378dd20d54354f809c2e
Aroooba <arooba.shahoor@gmail.com>
2024-04-30 07:46:53 +0900

    update for vervel config

--- django-blog/wsgi.py
+++ django-blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangonautic project.
+WSGI config for django-blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangonautic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-blog.settings")
 
 application = get_wsgi_application()
+
+app=application
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import os
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,6 +13,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Get the value of the PORT environment variable
+    port = os.environ.get('PORT')
+
+    if port is not None:
+        print(f"PORT environment variable value: {port}")
+    else:
+        print("PORT environment variable is not set")
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
